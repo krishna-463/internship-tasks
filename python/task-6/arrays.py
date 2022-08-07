9069829ed619e66e9407e0e046c1a553545e6b17
M Vamshi Krishna Reddy <102311673+krishna-463@users.noreply.github.com>
2022-08-07 11:11:25 +0530

    Create arrays.py

---
+++ python/task-6/arrays.py
@@ -0,0 +1,12 @@
+import numpy
+
+def arrays(arr):
+    a = numpy.array(arr[::-1],float)
+    return a
+    
+    # complete this function
+    # use numpy.array
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
